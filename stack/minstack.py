6d54cfd30fd99a708944acd3a02e74e4c96bd5a8
mikey6453 <tvq3bn@virginia.edu>
2026-01-15 22:37:49 -0500

    minstack done

--- stack/minstack.py
+++ stack/minstack.py
@@ -1,17 +1,67 @@
+# Minimum Stack
+# Medium
+# Company Tags
+# Hints
+# Design a stack class that supports the push, pop, top, and getMin operations.
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# Each function should run in 
+# O(1) time.
+
+# Example 1:
+
+
+# Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+
+# Output: [null,null,null,null,0,null,2,1]
+
+# Explanation:
+# MinStack minStack = new MinStack();
+# minStack.push(1);
+# minStack.push(2);
+# minStack.push(0);
+# minStack.getMin(); // return 0
+# minStack.pop();
+# minStack.top();    // return 2
+# minStack.getMin(); // return 1
+
+
 class MinStack:
 
     def __init__(self):
-        pass
+        self.stack = []
+        self.minstack = []
 
     def push(self, val: int) -> None:
-        pass
+        self.stack.append(val)
+
+        if not self.minstack or val <= self.minstack[-1]:
+            self.minstack.append(val)
+
 
     def pop(self) -> None:
-        pass
+        if self.stack[-1] == self.minstack[-1]:
+            self.minstack.pop()
+
+        self.stack.pop()
 
     def top(self) -> int:
-        return 0
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        pass
-        
+        return self.minstack[-1]
+
+
+obj = MinStack()
+
+print(obj.push(1))
+print(obj.push(2))
+print(obj.push(0))
+print(obj.getMin())
+print(obj.pop())
+print(obj.top())
+print(obj.getMin())
