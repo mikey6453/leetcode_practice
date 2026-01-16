b1763ff0d544615f8c3c33262cafaa190a604d22
mikey6453 <tvq3bn@virginia.edu>
2026-01-15 22:24:26 -0500

    minstack

---
+++ stack/minstack.py
@@ -0,0 +1,17 @@
+class MinStack:
+
+    def __init__(self):
+        pass
+
+    def push(self, val: int) -> None:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def top(self) -> int:
+        return 0
+
+    def getMin(self) -> int:
+        pass
+        
