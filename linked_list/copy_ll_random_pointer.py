4f2d0188e7236f8d06b62b4b680a411722c218d5
mikey6453 <tvq3bn@virginia.edu>
2026-04-25 20:30:16 -0400

    copy linked list with random pointer

---
+++ linked_list/copy_ll_random_pointer.py
@@ -0,0 +1,60 @@
+"""
+Copy Linked List with Random Pointer
+Medium
+Topics
+Company Tags
+Hints
+You are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
+
+Create a deep copy of the list.
+
+The deep copy should consist of exactly n new nodes, each including:
+
+The original value val of the copied node
+A next pointer to the new node corresponding to the next pointer of the original node
+A random pointer to the new node corresponding to the random pointer of the original node
+Note: None of the pointers in the new list should point to nodes in the original list.
+
+Return the head of the copied linked list.
+
+In the examples, the linked list is represented as a list of n nodes. Each node is represented as a pair of [val, random_index] where random_index is the index of the node (0-indexed) that the random pointer points to, or null if it does not point to any node.
+
+Example 1:
+
+
+
+Input: head = [[3,null],[7,3],[4,0],[5,1]]
+
+Output: [[3,null],[7,3],[4,0],[5,1]]
+"""
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldtocopy = {None: None}
+
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            oldtocopy[curr] = copy
+            curr = curr.next
+
+        curr = head
+        while curr:
+            copy = oldtocopy[curr]
+            copy.next = oldtocopy[curr.next]
+            copy.random = oldtocopy[curr.random]
+            curr = curr.next
+
+        return oldtocopy[head]
+
+
+# 1. Use a two pass solution: create a copy of each node until you reach null and map the original node to the deep copy node in a hashmap
+# 2. In the second pass, set the value of next and random respectively to where those parameters are pointing to in the original linked list using the hashmap
