ffd3780585412f40845f755e85aef60df1c4af05
mikey6453 <tvq3bn@virginia.edu>
2026-05-23 04:43:28 -0400

    is same tree

---
+++ trees/is_same_tree.py
@@ -0,0 +1,57 @@
+"""
+Same Binary Tree
+Easy
+Topics
+Company Tags
+Hints
+Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+
+
+
+Input: p = [1,2,3], q = [1,2,3]
+
+Output: true
+Example 2:
+
+
+
+Input: p = [4,7], q = [4,null,7]
+
+Output: false
+Example 3:
+
+
+
+Input: p = [1,2,3], q = [1,3,2]
+
+Output: false
+Constraints:
+
+0 <= The number of nodes in both trees <= 100.
+-100 <= Node.val <= 100
+"""
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val: int, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]):
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
