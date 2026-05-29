bdd9a1ccc136f6cb8b2d815c6c6c55ebbcf8557a
mikey6453 <tvq3bn@virginia.edu>
2026-05-29 03:50:27 -0400

    dfs flood fill

---
+++ graph/dfs/image_flood.py
@@ -0,0 +1,53 @@
+"""
+Flood Fill
+Easy
+Topics
+Company Tags
+You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill:
+
+Begin with the starting pixel and change its color to color.
+Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+The process stops when there are no more adjacent pixels of the original color to update.
+Return the modified image after performing the flood fill.
+
+Example 1:
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation:
+
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+
+Output: [[0,0,0],[0,0,0]]
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows, col = len(image), len(image[0])
+        start = image[sr][sc]
+        if start == color:
+            return image
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= col or image[r][c] != start:
+                return
+
+            image[r][c] = color
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
