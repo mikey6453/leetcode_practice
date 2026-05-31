3152481d827e698dbabdd4df36d52b9254f0d101
mikey6453 <tvq3bn@virginia.edu>
2026-05-30 20:28:07 -0400

    kth smallest interger

---
+++ trees/kth_smallest_integer_bt.py
@@ -0,0 +1,46 @@
+"""
+Kth Smallest Integer in BST
+Medium
+Topics
+Company Tags
+Hints
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) in the tree.
+
+A binary search tree satisfies the following constraints:
+
+The left subtree of every node contains only nodes with keys less than the node's key.
+The right subtree of every node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees are also binary search trees.
+Example 1:
+
+
+
+Input: root = [2,1,3], k = 1
+
+Output: 1
+"""
+
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        order = []
+
+        def dfs(root):
+            if not root:
+                return
+
+            dfs(root.left)
+            order.append(root.val)
+            dfs(root.right)
+        
+        dfs(root)
+        return order[k-1]
