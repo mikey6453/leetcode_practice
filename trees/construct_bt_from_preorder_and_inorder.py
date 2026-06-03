d65b795edd52fdc8c6c9365c18d67afd2ca8ee9f
mikey6453 <tvq3bn@virginia.edu>
2026-06-03 06:23:11 -0400

    construct BT from preorder and inorder traversal

---
+++ trees/construct_bt_from_preorder_and_inorder.py
@@ -0,0 +1,44 @@
+"""
+Construct Binary Tree from Preorder and Inorder Traversal
+Medium
+Topics
+Company Tags
+Hints
+You are given two integer arrays preorder and inorder.
+
+preorder is the preorder traversal of a binary tree
+inorder is the inorder traversal of the same tree
+Both arrays are of the same size and consist of unique values.
+Rebuild the binary tree from the preorder and inorder traversals and return its root.
+
+Example 1:
+
+
+
+Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
+
+Output: [1,2,3,null,null,null,4]
+"""
+
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
