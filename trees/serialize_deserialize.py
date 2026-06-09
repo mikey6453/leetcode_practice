74282d18b979dc0c1d7d608b617135c1b0dbb508
mikey6453 <tvq3bn@virginia.edu>
2026-06-09 04:48:13 -0400

    serialize deserialize binary tree

---
+++ trees/serialize_deserialize.py
@@ -0,0 +1,73 @@
+"""
+Serialize and Deserialize Binary Tree
+Hard
+Topics
+Company Tags
+Hints
+Implement an algorithm to serialize and deserialize a binary tree.
+
+Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across a network to be reconstructed later in another computer environment.
+
+You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure. There is no additional restriction on how your serialization/deserialization algorithm should work.
+
+Note: The input/output format in the examples is the same as how NeetCode serializes a binary tree. You do not necessarily need to follow this format.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,null,4,5]
+
+Output: [1,2,3,null,null,4,5]
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional
+
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        
+        def dfs(root):
+            if not root:
+                res.append("N")
+                return
+
+            res.append(str(root.val))
+
+            dfs(root.left)
+            dfs(root.right)
+
+        dfs(root)
+        return ",".join(res)
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+
+            node.left = dfs()
+            node.right = dfs()
+
+            return node
+
+        return dfs()
